692c56a5378e34c6e610fb36f4771b0ad936d5e0
Kevin Krouse <291029+kevinkrouse@users.noreply.github.com>
2024-09-26 15:56:17 -0700

    switch to updated syntax

--- src/town/lilac/humble/app/main.clj
+++ src/town/lilac/humble/app/main.clj
@@ -12,11 +12,12 @@
 
 (def app
   "Main app definition."
-  (ui/default-theme ; we must wrap our app in a theme
-   {}
-   ;; just some random stuff
-   (ui/center
-    (ui/label "hi"))))
+  (fn []
+    [ui/default-theme ; we must wrap our app in a theme
+     {}
+     ;; just some random stuff
+     [ui/center
+      [ui/label "hi"]]]))
 
 ;; reset current app state on eval of this ns
 (reset! state/*app app)
